283a18a8df403ca019386ab3fd84968ad5a03ab1
mirko-geno <mirkogenovesi24@gmail.com>
2024-12-08 19:39:07 -0300

    WASM / WASI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use AR_Rust::Stream;
+use opencv::highgui;
+
+const MODEL_PATH: &str = "hand_landmarker.task";
+
 fn main() {
-    println!("Hello, world!");
+    // Create a new stream with the model path
+    let mut stream = Stream::new(7, MODEL_PATH);
+
+    // Loop to capture video and process it
+    while highgui::wait_key(1).unwrap() != 27 { // Exit on 'Esc'
+        stream.update(); // Update stream with new frames
+        stream.detect_and_draw_landmarks(); // Detect and draw landmarks
+        stream.show(); // Display the processed frame
+    }
 }
